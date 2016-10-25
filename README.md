e7ff8be65411f8c4296e2ec05b33642ea2bf5b8f
Mateo Buitrago <m.buitrago@pappcorn.com>
2016-10-25 13:08:01 -0500

    feat: Refactor code to be able to load true JSON files. - Use localStorage - Use jQuery.getJSON

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Easy certificate download
 
-Simple website to host downloadable certificates, created using **InDesign Data Merge Tool**, CSVtoJSON, HTML, CSS and Vanilla JS.
+Simple website to host downloadable certificates, created using **InDesign Data Merge Tool**, [CSVtoJSON](http://www.convertcsv.com/csv-to-json.htm), and Knockout JS.
 
 ## Getting started
 
-Required files:
+### Required files:
 * Certificates files. (If you have a database you can create them with **InDesign Data Merge Tool** and this [awesome script.](https://colecandoo.com/2015/09/04/data-merge-from-indesign-to-unique-filenames-now-a-reality/))
 * JSON file with certificate names. (You should create it from the same database)
 
@@ -12,5 +12,11 @@ Required files:
 
 Filename: `MATEO BUITRAGO.pdf`
 JSON: `{
-	name: `MATEO BUITRAGO`
+	name: MATEO BUITRAGO
 }`
+
+### Steps
+
+1. Upload certificate files and JSON file to the certificate folder.
+2. Link JSON file in the HTML
+3. Done!
--- js/app.js
+++ js/app.js
@@ -1,18 +1,32 @@
-ko.utils.stringStartsWith = function (string, startsWith) {        	
-            string = string || "";
-            if (startsWith.length > string.length)
-                return false;
-            return string.substring(0, startsWith.length) === startsWith;
-}
+(function(){
+    $.getJSON("certificates/asistentesLGBT.json", function(data){
+        if(!localStorage.asistentes){
+                localStorage.asistentes = JSON.stringify(data);
+                ko.applyBindings(viewmodel);
+        }
+    });
+
+    ko.utils.stringStartsWith = function (string, startsWith) {
+        console.log("string");      
+        string = string || "";
+        if (startsWith.length > string.length)
+            return false;
+        return string.substring(0, startsWith.length) === startsWith;
+    }
+})();
 
 var viewmodel = function(){
     var self = this;
     self.asistentesLgbt = ko.observableArray();
+
+    var asistentes = JSON.parse(localStorage.asistentes);
+
+    console.log(asistentes);
     
     asistentes.forEach(function(item){
         self.asistentesLgbt.push({
-            'name' : item.Uppercase,
-            'url' : './certificates/' + item.Uppercase + '.pdf'
+            'name' : item.asistente,
+            'url' : './certificates/' + item.asistente + '.pdf'
         });
     });
     
@@ -29,4 +43,6 @@ var viewmodel = function(){
     })
 }
 
+
 ko.applyBindings(viewmodel);
+
